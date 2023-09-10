6de83d32eaf41e911bf9423278c6e0add6a32e26
natmacira <natimciraolo@gmail.com>
2023-09-10 18:16:58 +0200

    favicon added

--- header.php
+++ header.php
@@ -26,7 +26,7 @@ $base_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : 
 		var base_url = "<?php echo $base_url; ?>";
 	</script>
 	<script src="js/script.js?v=<?php echo SITE_VERSION; ?>"></script>
-	<!-- <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
 	<link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png">
 	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
 	<link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-icon-76x76.png">
@@ -40,15 +40,15 @@ $base_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : 
 	<link rel="icon" type="image/png" sizes="96x96" href="img/favicon/favicon-96x96.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
 	<link rel="manifest" href="img/favicon/manifest.json">
-	<meta name="msapplication-TileColor" content="#0066ff">
+	<meta name="msapplication-TileColor" content="#ffffff">
 	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-	<meta name="theme-color" content="#0066ff">
-	<meta itemProp="image" property="og:image" content="https://nodo.org.ar/img/favicon/android-icon-192x192.png" />
+	<meta name="theme-color" content="#ffffff">
+
 	<meta property="og:description" content="Combatimos la inseguridad alimentaria impactando positivamente en el medio ambiente.">
 	<meta property="og:locale" content="es_LA">
 	<meta property="og:image" content="https://nodo.org.ar/img/favicon/android-icon-192x192.png" />
 	<meta property="og:type" content="website">
-	<meta property="og:url" content="https://nodo.org.ar/" /> -->
+	<meta property="og:url" content="https://nodo.org.ar/" />
 </head>
 
 <body class="<?php echo $body_class ?>">
