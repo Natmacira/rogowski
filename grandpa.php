17ce5403e1271057d788276be941492aa12d10cd
natmacira <natimciraolo@gmail.com>
2023-09-10 19:10:59 +0200

    fixing details from testing

--- grandpa.php
+++ grandpa.php
@@ -15,9 +15,9 @@ require_once 'header.php';
     </ul>
 </section>
 <section>
-    <img src="img/Granddad/1_granddad.JPG" alt="">
+    <img src="img/Granddad/1_granddad.jpg" alt="">
     <img src="img/Granddad/2_granddad.jpg" alt="">
-    <img src="img/Granddad/3_granddad.JPG" alt="">
+    <img src="img/Granddad/3_granddad.jpg" alt="">
     <h1>‘Granddad’s Archive’</h1>
     <p>
         Before he passed away, I worked with my granddad Czesław Możejko on his archive of photography from Silesia, Poland and the Eastern Bloc from 1950-1980. The project is tentatively called ‘Granddad’s Archive’. Photographs presented here are original double exposures from the period.
--- wapien.php
+++ wapien.php
@@ -23,7 +23,9 @@ require_once 'header.php';
     <img src="img/Wapien/5_wapien.jpg" alt="">
     <p>
     <h1>‘Wapień’</h1>
+    <p>
     Wapień means limestone in Polish. All photographs, with the exception of (i), were taken throughout the months of March and April 2020 during in the first lockdown. The portraits are a result of collaboration with the subjects, friends with whom I shared the covid isolation bubble. I took these photographs during our discussions about what the future might look like. The project was an a prompt attempt to process the fast changing reality and the various contradictory emotions within myself and my collaborators. With the exception of (i) every photograph was shot in our shared bathroom.
+    </p>
     <br><br><br>
      </p>
 </section>
