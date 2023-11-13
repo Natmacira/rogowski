08629d9a2ce7e15de066cb87989b99fc0ff8c823
natmacira <natimciraolo@gmail.com>
2023-11-13 14:57:04 +0100

    updates home images

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
 
+// window.addEventListener('load', function () {
+
+//        // Random home
+
+//     var img = ["img/Americana_light/a2.jpg", "img/film/argentina-home.jpg", "img/film/california-home.jpg", "img/Granddad/1_granddad.jpg", "img/Travels_in_reality/3_travels.jpg", "img/Wapien/wapien-home.jpg"]
+//     var link = ["/americana-light.php", "/film.php", "/film.php", "/grandpa.php", "/travels-in-reality.php", "/wapien.php"]
+//     function imgRandom(imgArr) {
+//         var randomIndex = Math.floor(Math.random() * imgArr.length);
+//         return {
+//             img: imgArr[randomIndex],
+//             link: link[randomIndex]
+//         };
+//     }
+//     console.log('hola2');
+
+//     var randomImage = imgRandom(img);
+//     document.getElementById("random-image").src = randomImage.img.toString();
+//     document.getElementById("random-image-link").href = randomImage.link.toString();
+
+
+
+// });
+
+
+
 window.addEventListener('load', function () {
+    var img = ["img/Americana_light/a2.jpg", "img/film/argentina-home.jpg", "img/film/california-home.jpg", "img/Granddad/granddad-home.jpg", "img/Travels_in_reality/3_travels.jpg", "img/Wapien/wapien-home.jpg"];
+    var link = ["/americana-light.php", "/film.php", "/film.php", "/grandpa.php", "/travels-in-reality.php", "/wapien.php"];
 
-       // Random home
-
-    var img = ["img/Americana_light/a2.jpg", "img/film/2_argentina.jpg", "img/film/1_california.jpg", "img/Granddad/1_granddad.jpg", "img/Travels_in_reality/3_travels.jpg", "img/Wapien/5_wapien.jpg"]
-    var link = ["/americana-light.php", "/film.php", "/film.php", "/grandpa.php", "/travels-in-reality.php", "/wapien.php"]
     function imgRandom(imgArr) {
         var randomIndex = Math.floor(Math.random() * imgArr.length);
         return {
@@ -12,16 +35,23 @@ window.addEventListener('load', function () {
             link: link[randomIndex]
         };
     }
-    console.log('hola2');
 
     var randomImage = imgRandom(img);
-    document.getElementById("random-image").src = randomImage.img.toString();
+    var randomImageElement = document.getElementById("random-image");
+    randomImageElement.src = randomImage.img.toString();
+    
+    // Elimina todas las clases actuales del elemento img
+    randomImageElement.removeAttribute('class');
+    
+    // Agrega la nueva clase con el nombre de la imagen
+    randomImageElement.classList.add(getImageClassName(randomImage.img));
     document.getElementById("random-image-link").href = randomImage.link.toString();
-
-
-
 });
 
-
+function getImageClassName(imageUrl) {
+    var filename = imageUrl.split('/').pop();
+    var imageName = filename.replace(/\.[^/.]+$/, "");
+    return imageName;
+}
 
 
